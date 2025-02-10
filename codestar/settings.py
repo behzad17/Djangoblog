aa2df2dc14de851e8846aa7f4793831646c54c6f
BJ <158008244+behzad17@users.noreply.github.com>
2025-02-10 00:08:47 +0000

    uppdate database information

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-behzad17-djangoblog-0n6g7bsl8tl.ws.codeinstitute-ide.net',
