31c5626bf39acc33668f42dceacd9091a98304f5
BJ <158008244+behzad17@users.noreply.github.com>
2025-02-12 15:29:05 +0000

    add allauth adress to urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
     path('<slug:slug>/delete_comment/<int:comment_id>',
-         views.comment_delete, name='comment_delete'),     
+         views.comment_delete, name='comment_delete'),
+    path('accounts/', include('allauth.urls')),     
 ]
+    
