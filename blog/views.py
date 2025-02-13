7185c355ce84eca433e83212715bf8d01358c59e
BJ <158008244+behzad17@users.noreply.github.com>
2025-02-13 00:23:15 +0000

    change columns to 4 in index.html and some related changes on base.html

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ from .forms import CommentForm
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 8
 
 def post_detail(request, slug):
     """
