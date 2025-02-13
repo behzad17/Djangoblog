33272fc6b4983a198e467559719b0a60e2ecc524
BJ <158008244+behzad17@users.noreply.github.com>
2025-02-13 01:28:31 +0000

    add comment count to Home page

--- blog/views.py
+++ blog/views.py
@@ -3,29 +3,81 @@ from django.views import generic
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from .models import Post, Comment, Favorite
 from .forms import CommentForm
 
+class PostList(generic.ListView):
+    template_name = "blog/index.html"
+    paginate_by = 8
+
+def get_queryset(self):
+    return Post.objects.filter(status=1).annotate(comment_count=Count('comments'))
+
+# ✅ اصلاح post_detail
+def post_detail(request, slug):
+    """
+        نمایش جزئیات هر پست
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+    is_favorite = Favorite.objects.filter(user=request.user, post=post).exists() if request.user.is_authenticated else False
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+    else:
+        comment_form = CommentForm()
+
+    return render(
+        request,
+        "blog/post_detail.html",
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+            "comment_form": comment_form,
+            "is_favorite": is_favorite,
+        },
+    )
+
+"""
 # Create your views here.
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 8
 
+def get_queryset(self):
+    return Post.objects.filter(status=1).annotate(comment_count=Count('comments'))
+
 def post_detail(request, slug):
     """
-    Display an individual :model:`blog.Post`.
 
-    **Context**
+    
+    #Display an individual :model:`blog.Post`.
 
-    ``post``
-        An instance of :model:`blog.Post`.
+    #**Context**
 
-    **Template:**
+    #``post``
+        #An instance of :model:`blog.Post`.
 
-    :template:`blog/post_detail.html`
-    """
+    #**Template:**
 
+    #:template:`blog/post_detail.html`
+    
+"""
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
@@ -56,7 +108,7 @@ def post_detail(request, slug):
             "is_favorite": is_favorite,
         },
     )    
-
+"""
 def comment_edit(request, slug, comment_id):
     """
     view to edit comments
