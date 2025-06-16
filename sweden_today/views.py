21e58a7e8307d2c7dd48bc8e4020700363e05eb6
Behzad Javadian <behzadjavadian49@gmail.com>
2025-06-16 12:32:56 +0200

    fix: Correct .gitignore for __pycache__

--- sweden_today/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-
-def handler404(request, exception):
-    """
-    Custom 404 error handler.
-    Renders the 404.html template when a page is not found.
-    """
-    return render(request, '404.html', status=404) 
