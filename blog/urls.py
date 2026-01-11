62f9ad000156b4002559123e9e330d5f7d81e44b
Behzad Javadian <behzad_1349@hotmail.com>
2026-01-11 21:12:20 +0100

    Fix NoReverseMatch error for Persian slugs in URLs
    Issue: 500 error on homepage - NoReverseMatch for Persian slugs
    - URL pattern uses <slug:slug> which only allows ASCII characters
    - Persian slugs contain Unicode characters
    - Error: Reverse for 'post_detail' with Persian slug not found
    
    Solution:
    - Changed <slug:slug> to <str:slug> for post-related URLs
    - This allows Unicode characters (Persian/Farsi) in slugs
    - Category URLs still use <slug:category_slug> (categories are in English)
    
    This fixes the 500 error on homepage when posts have Persian slugs.

--- blog/urls.py
+++ blog/urls.py
@@ -12,9 +12,10 @@ urlpatterns = [
     path('remove-from-favorites/<int:post_id>/', views.remove_from_favorites, name='remove_from_favorites'),
     path('like-post/<int:post_id>/', views.like_post, name='like_post'),
     path('complete-setup/', views.complete_setup, name='complete_setup'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('<slug:slug>/edit/', views.edit_post, name='edit_post'),
-    path('<slug:slug>/delete/', views.delete_post, name='delete_post'),
-    path('<slug:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
-    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
+    # Use str instead of slug to allow Unicode characters (Persian/Farsi)
+    path('<str:slug>/', views.post_detail, name='post_detail'),
+    path('<str:slug>/edit/', views.edit_post, name='edit_post'),
+    path('<str:slug>/delete/', views.delete_post, name='delete_post'),
+    path('<str:slug>/edit_comment/<int:comment_id>/', views.comment_edit, name='comment_edit'),
+    path('<str:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ]
